24920cc0c8db9874993a85fab143f6ac83ff5baa
AnvarTurobov <anvar.turobov@gmail.com>
2015-07-14 15:48:59 +0100

    got rid of minor bugs, implemented coundown

--- app.js
+++ app.js
@@ -1,117 +1,127 @@
-// arithmetics game - where a random arithmetic operation will be shown, 
-// where operator will be hidden. at the bottom of the equation, 
-// arithmetic operators will be shown only for few seconds and changed to "?" sign.
-// user should first understand what operator is used for the the equation and select the right operator.
-//
-// 1- build a page where with a display to - show the level- indicate win/lose status
-// 2- window to show the equation 
-// 3- for separate containers to show/hide arithmetic operators
-// message to welcome the user into the game as well as instructions- should have a start button
-// blanc page with dedicated windows for the level status, equation, 4x equation operators 
-// blanc page should have a countdown to show the content for a spesific time and hide some of the content
-// user makes a choice && computer will compare the results if true- { users goes to the next level
-// next level will be similar- the only difference would be, the time to show the content is less
+var $display;
+var roundsPlayed= [];
+var time = 3000;
+
+var initialize = function(){
+	$display = $('#display');
+	bindEvents();
+}
+
+var randomNumber = function(min,max) {
+	return parseInt(Math.floor(Math.random()*(max-min+1)+min));
+}
+
+var randomOperator = function() {
+	var operators = ["+", "-", "/", "*"];
+	return operators[Math.floor(Math.random() * operators.length)]
+}
+
+var generateEquation = function() {
+	var min = 1,
+	max = 100,
+	firstRandomNumber = randomNumber(min, max),
+	secondRandomNumber = randomNumber(min, max),
+	operator = randomOperator(),
+	answer;
+
+	switch (operator) {
+		case "+": 
+		answer = firstRandomNumber + secondRandomNumber;
+		break;
+		case "-": 
+		answer = firstRandomNumber - secondRandomNumber;
+		break;
+		case "/": 
+		answer = firstRandomNumber / secondRandomNumber;
+		break;
+		case "*": 
+		answer = firstRandomNumber * secondRandomNumber;
+		break;
+	}
+
+	return answer = {
+		firstRandomNumber: firstRandomNumber,
+		operator: operator,
+		secondRandomNumber: secondRandomNumber,
+		answer: answer
+	}
+}
+
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+cancel = true;
+
+function startTimer(duration, display) {
+	var timer = duration, minutes, seconds;
+
+	var interval = setInterval(function () {
+		seconds = parseInt(timer % 60, 10);
+		seconds = seconds < 10 ? seconds : seconds;
+
+		display.text(seconds);
+		console.log(timer)
 
 
- var $display;
- var roundsPlayed= [];
- var time = 3000;
-
- var initialize = function(){
- 	$display = $('#display');
- 	bindEvents();
- }
-
- var randomNumber = function(min,max) {
- 	return parseInt(Math.floor(Math.random()*(max-min+1)+min));
- }
-
- var randomOperator = function() {
- 	var operators = ["+", "-", "/", "*"];
- 	return operators[Math.floor(Math.random() * operators.length)]
- }
-
- var generateEquation = function() {
- 	var min = 1,
- 	max = 100,
- 	firstRandomNumber = randomNumber(min, max),
- 	secondRandomNumber = randomNumber(min, max),
- 	operator = randomOperator(),
- 	answer;
-
- 	switch (operator) {
- 		case "+": 
- 		answer = firstRandomNumber + secondRandomNumber;
- 		break;
- 		case "-": 
- 		answer = firstRandomNumber - secondRandomNumber;
- 		break;
- 		case "/": 
- 		answer = firstRandomNumber / secondRandomNumber;
- 		break;
- 		case "*": 
- 		answer = firstRandomNumber * secondRandomNumber;
- 		break;
- 	}
-
- 	return answer = {
- 		firstRandomNumber: firstRandomNumber,
- 		operator: operator,
- 		secondRandomNumber: secondRandomNumber,
- 		answer: answer
- 	}
- }
-
- function shuffle(array) {
- 	var currentIndex = array.length, temporaryValue, randomIndex ;
-
-   // While there remain elements to shuffle...
-   while (0 !== currentIndex) {
-
-     // Pick a remaining element...
-     randomIndex = Math.floor(Math.random() * currentIndex);
-     currentIndex -= 1;
-
-     // And swap it with the current element.
-     temporaryValue = array[currentIndex];
-     array[currentIndex] = array[randomIndex];
-     array[randomIndex] = temporaryValue;
-   }
-
-   return array;
- }
-
- var easyGame = function(){
- 	$('#easy').addClass('animated rubberBand');
- 	setTimeout(function() {
- 		$('#easy').removeClass('animated rubberBand');
- 	}, 1000)
-
- 	var operators = ["+", "-", "/", "*"];
- 	for(var i = 1; i<5; i++){
- 		$('#operator'+i).html(newShuffle = shuffle(operators).splice(0, 1))
- 		$('#operator'+i).attr("data-id", newShuffle.splice(0, 1))
- 	}
+		if (timer == -1) {
+			display.text("");
+			clearInterval(interval)
+			easyGame();
+		} else if(--timer < -1) {
+			display.text("");
+		}
+	},1000);
+}
 
 
- 	event.preventDefault();
+var easyGame = function(){
+	$('#easy').addClass('animated rubberBand');
+
+	setTimeout(function() {
+		$('#easy').removeClass('animated rubberBand');
+	}, 2000)
+
+	var operators = ["+", "-", "/", "*"];
+	for(var i = 1; i<5; i++){
+		$('#operator'+i).html(newShuffle = shuffle(operators).splice(0, 1))
+		$('#operator'+i).attr("data-id", newShuffle.splice(0, 1))
+	}
+
  	var answer = generateEquation();
- 	if (answer.answer !== Math.round(answer.answer)) {
+ 	if ((answer.answer !== Math.round(answer.answer)) || (answer.answer.toString().indexOf("-") >= 0)) {
+ 		debugger
  		answer = generateEquation();
  	}
+
 
  	$display.text(answer.firstRandomNumber + " " + "?" + " "+ answer.secondRandomNumber + " = " + answer.answer)
 
  	$(".operator").on("click", function(){
  		if(this.getAttribute("data-id") === answer.operator){
- 			$('#progress').html('Correct! Верно! Кichtig! 正確! Correctto!<br><button id="continue" class="animate shake">CONTINUE</button>');
+ 			$('#progress').html('Correct! Верно! Кichtig! 正確! Correctto!<br><button id="continue">CONTINUE</button>');
  			$("#continue").on("click", function(){
  				$('#progress').html("");
  				roundsPlayed++;
- 				time -= 400;
+ 				time -= 200;
  			})  
  		} else {
- 		$('#progress').html('WRONG <br> Come on man, you can do better than that! Неправильно! Falsch! 錯! Sbagliato! <br> <button id="startAgain">START AGAIN ===></button>'); //alert("wrong answer")
+ 		$('#progress').html('WRONG! come one, you can do better than that! Неправильно! Falsch! 錯! Sbagliato!<br><button id="startAgain">START AGAIN</button>'); //alert("wrong answer")
 
  		$("#startAgain").on("click", function(){
  			$('#progress').html("");
@@ -120,6 +130,7 @@
  	
  })
 
+
  	setTimeout(function() {
  		$('.operator').html('?')
  	}, time);
@@ -127,15 +138,20 @@
  }
 
  var bindEvents = function() {
- 	$('#easy').on('click', easyGame);
- 	$('#progress').on('click', easyGame);
+ 	var three_seconds = 3,
+ 	display = $("#display");
+ 	
+ 	$('#easy').on('click', function(){
+ 		startTimer(three_seconds, display)
+ 	});
+
+ 	$('#progress').on('click', function(){
+ 		$('#display').html("")
+ 		startTimer(three_seconds, display)
+ 	});
  }
 
 
  $(function() {
  	initialize();
  });
-
-
-
-
---
+++ pseudocode.md
@@ -0,0 +1,13 @@
+// arithmetics game - where a random arithmetic operation will be shown, 
+// where operator will be hidden. at the bottom of the equation, 
+// arithmetic operators will be shown only for few seconds and changed to "?" sign.
+// user should first understand what operator is used for the the equation and select the right operator.
+//
+// 1- build a page where with a display to - show the level- indicate win/lose status
+// 2- window to show the equation 
+// 3- for separate containers to show/hide arithmetic operators
+// message to welcome the user into the game as well as instructions- should have a start button
+// blanc page with dedicated windows for the level status, equation, 4x equation operators 
+// blanc page should have a countdown to show the content for a spesific time and hide some of the content
+// user makes a choice && computer will compare the results if true- { users goes to the next level
+// next level will be similar- the only difference would be, the time to show the content is less
